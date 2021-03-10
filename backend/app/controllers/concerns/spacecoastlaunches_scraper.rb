5e3f0d765c43e9f58650fd5bb83f2e6cc02b7131
tscotce <tatyana.celovsky@gmail.com>
2021-03-10 15:29:31 -0500

    Update SpacecoastlaunchesScraper

--- backend/app/controllers/concerns/spacecoastlaunches_scraper.rb
+++ backend/app/controllers/concerns/spacecoastlaunches_scraper.rb
@@ -1,9 +1,17 @@
+require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 module SpacecoastlaunchesScraper
     def scrape
     html ||= open("https://spacecoastlaunches.com/launch-list/")
-    res = Nokogiri::HTML(html)
-    end
     
+    doc = Nokogiri::HTML(html)
+
+        doc.css(".sc-launch__title--secondary").each do |launch|
+            launch = Launch.new
+            date = launch.css.text
+        end
+    end
+
 end
