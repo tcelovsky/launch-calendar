7e8dff2935d45fd00090871bde2abc84a97473b9
tscotce <tatyana.celovsky@gmail.com>
2021-04-08 18:08:30 -0400

    Create launchesReducer.js

---
+++ frontend/src/reducers/launchesReducer.js
@@ -0,0 +1,19 @@
+const launchesReducer = (state = {
+    launches: [],
+    loading: false
+}, action) => {
+    switch(action.type) {
+        case 'LOADING_LAUNCHES':
+            return {
+                ...state, launches: [...state.launches], loading:true
+            }
+        case 'ADD_LAUNCHES':
+            return {
+                ...state, launches: action.launches, loading: false
+            }
+        default: 
+            return state;
+    }
+}
+
+export default launchesReducer;
