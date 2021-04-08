4d3f8ca3c20eb6f3e4f70e975c400ac04c0e4bde
tscotce <tatyana.celovsky@gmail.com>
2021-04-08 18:33:57 -0400

    Udate return statement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
@@ -14,20 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">Launch Calendar</header>
       </div>
     );
   }
