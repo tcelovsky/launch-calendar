337327fb439669a008bc3c53ffc6fbb7446af371
tscotce <tatyana.celovsky@gmail.com>
2021-04-09 15:17:48 -0400

    Clean up unused methods

--- frontend/src/components/LaunchCard.js
+++ frontend/src/components/LaunchCard.js
@@ -3,15 +3,10 @@ import CardFront from './CardFront';
 import CardBack from './CardBack';
 
 class LaunchCard extends Component {
-
-    // generateLaunchCards = () => {
-    //     return this.props.launches.map(launch => launch={launch})
-    // }
     
     render() {
         return (
             <div className="launch-card">
-                {/* {console.log(this.generateLaunchCards())} */}
                 <CardFront />
                 <CardBack />
             </div>
