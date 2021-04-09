29a82af23f7b66b4abcc5862c8736e6d63ea6653
tscotce <tatyana.celovsky@gmail.com>
2021-04-09 15:32:16 -0400

    Create CardFront

--- frontend/src/components/CardFront.js
+++ frontend/src/components/CardFront.js
@@ -4,8 +4,13 @@ class CardFront extends Component {
 
     render() {
         return (
-            <div className='card-front'>
-                {console.log(this.props)}
+            <div className="card-front">
+                <ul>
+                    <li>{this.props.date}</li>
+                    <li>{this.props.time}</li>
+                    <li>{this.props.site}</li>
+                    <li>{this.props.rocket}</li>
+                </ul>
             </div>
         )
     }
