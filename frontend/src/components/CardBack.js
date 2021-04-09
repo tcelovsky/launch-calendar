8c8df2ff2f47da418fec81377cf6545ffafd9f5d
tscotce <tatyana.celovsky@gmail.com>
2021-04-09 14:58:33 -0400

    Create basic structure

---
+++ frontend/src/components/CardBack.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CardBack extends Component {
+
+    render() {
+        return (
+            <div className='card-back'>
+                {console.log(this.props)}
+            </div>
+        )
+    }
+}
+
+export default CardBack;
