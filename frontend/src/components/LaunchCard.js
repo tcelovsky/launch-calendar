06206ec1c07b6533504107e505e7799d1fc78637
tscotce <tatyana.celovsky@gmail.com>
2021-04-09 14:49:46 -0400

    Add basic setup to LaunchCard.js

--- frontend/src/components/LaunchCard.js
+++ frontend/src/components/LaunchCard.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class LaunchCard extends Component {
-
+    
+    render() {
+        return (
+            <div className="launch-card">
+                {console.log(this.props)}
+            </div>
+        )
+    }
 }
 
 export default LaunchCard;
