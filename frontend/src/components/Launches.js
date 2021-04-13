d93679fd511529e58d6c3e1097f7754194736456
tscotce <tatyana.celovsky@gmail.com>
2021-04-13 18:08:02 -0400

    Add links for each launch card

--- frontend/src/components/Launches.js
+++ frontend/src/components/Launches.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import LaunchCard from './LaunchCard';
 
 class Launches extends Component {
 
     generateLaunchCards = () => {
-        return this.props.launches.map(launch => <LaunchCard key={launch.id} launch={launch} />)
+        return this.props.launches.map(launch => 
+            // <LaunchCard key={launch.id} launch={launch} />
+            <Link key={launch.id} to={`/launches/${launch.id}`}>
+                {<LaunchCard key={launch.id} launch={launch} />}
+            </Link>
+        )
     }
 
     render() {
