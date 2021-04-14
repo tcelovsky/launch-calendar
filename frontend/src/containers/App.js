4a08d7b142d448358191f8dd9fe4e9c0ac2ed293
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 10:16:09 -0400

    Move App to Containers folder

---
+++ frontend/src/containers/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchLaunches } from './actions/launchActions';
+import Launches from './components/Launches';
+import NavBar from './components/NavBar';
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchLaunches()
+  }
+  
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Launch Calendar</h1>
+            <NavBar />
+          </header>
+          <Route exact path='/' render={routerProps =>
+	          <Launches {...routerProps} launches={this.props.launches}/>} />
+          <Route path='/launches' render={routerProps =>
+	          <Launches {...routerProps} launches={this.props.launches}/>} />
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    launches: state.launches,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchLaunches: () => dispatch(fetchLaunches())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
