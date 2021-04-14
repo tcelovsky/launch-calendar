20834150cb590f163b38e8dd5f9bd744f5ba8682
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 16:07:11 -0400

    Update fields based on props

--- frontend/src/components/AddToCalendarButton.js
+++ frontend/src/components/AddToCalendarButton.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddToCalendarButton = () => {
-  return (
-    <div title="Add to Calendar" class="addeventatc">
+class AddToCalendarButton extends Component {
+  
+  render() {
+    const date = this.props.date
+    const time = this.props.time
+
+    return (
+        <div title="Add to Calendar" className="addeventatc">
         Add to Calendar
-        <span class="start">04/28/2021 08:00 AM</span>
-        {/* <span class="end">04/28/2021 10:00 AM</span> */}
-        <span class="timezone">America/New_York</span>
-        <span class="title">Summary of the event</span>
-        <span class="description">Description of the event</span>
-        {/* <span class="location">Location of the event</span> */}
-    </div>
-  );
-};
+            <span className="start">04/28/2021 08:00 AM</span>
+            <span className="timezone">America/New_York</span>
+            <span className="title">{this.props.rocket} Launch</span>
+            <span className="description">{this.props.mission} from {this.props.site}.</span>
+        </div>
+    );
+    };
+}
 
-export default About;
+export default AddToCalendarButton;
