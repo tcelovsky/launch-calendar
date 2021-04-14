27e64158e896595ec86c137a25eeb2de021e0c67
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 15:21:34 -0400

    Create basic structure

---
+++ frontend/src/components/AddToCalendarButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AddToCalendarButton = () => {
+  return (
+    <div title="Add to Calendar" class="addeventatc">
+        Add to Calendar
+        <span class="start">04/28/2021 08:00 AM</span>
+        {/* <span class="end">04/28/2021 10:00 AM</span> */}
+        <span class="timezone">America/New_York</span>
+        <span class="title">Summary of the event</span>
+        <span class="description">Description of the event</span>
+        {/* <span class="location">Location of the event</span> */}
+    </div>
+  );
+};
+
+export default About;
