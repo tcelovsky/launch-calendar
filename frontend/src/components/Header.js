1cdf073233d823bd376ef25767d1f840b0b90c02
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 11:00:30 -0400

    Create Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+        <h1>Launch Calendar</h1>
+    </header>
+  );
+};
+
+export default Header;
