894492e1869fadde373e843e23222ea644f0efd2
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 17:39:35 -0400

    Add parseDate and parseTime methods to format date and time for the calendar button

--- frontend/src/components/AddToCalendarButton.js
+++ frontend/src/components/AddToCalendarButton.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { DateTime } from "luxon";
 
 class AddToCalendarButton extends Component {
   
+  parseDate = (date) => {
+      const dateInMillis = Date.parse(date)
+      return DateTime.fromMillis(dateInMillis).toISODate()
+  }
+
+  parseTime = (time) => {
+      switch(time) {
+            case 'TBD':
+                return '8:00 am'
+            default:
+                return time;
+      }
+  }
+
   render() {
-    const date = this.props.date
-    const time = this.props.time
+    const date = this.parseDate(this.props.date)
+    const time = this.parseTime(this.props.time)
 
     return (
         <div title="Add to Calendar" className="addeventatc">
         Add to Calendar
-            <span className="start">04/28/2021 08:00 AM</span>
+            <span className="start">{date} {time}</span>
             <span className="timezone">America/New_York</span>
             <span className="title">{this.props.rocket} Launch</span>
             <span className="description">{this.props.mission} from {this.props.site}.</span>
