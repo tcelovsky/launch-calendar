aece673bfdcf28dadd6a99a2188403610cdc5018
tscotce <tatyana.celovsky@gmail.com>
2021-04-14 10:23:18 -0400

    Move index.css to new styles folder and update import in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './styles/index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
