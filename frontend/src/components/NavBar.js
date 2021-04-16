f8ef0840d2262e18c34d728147a52ba023d54460
tscotce <tatyana.celovsky@gmail.com>
2021-04-16 17:44:44 -0400

    Update className

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,11 +3,24 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <NavLink to='/' exact>Home</NavLink>
-      <NavLink to='/launches' exact>Launch Calendar</NavLink>
-      <NavLink to='/about' exact>About</NavLink>
-    </div>
+    <nav className="navbar navbar-expand-md">
+      {/* <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button> */}
+      <div className="collapse navbar-collapse" id="main-navigation">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/' exact>Home</NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink className="nav-link" to='/launches' exact>Launch Calendar</NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink className="nav-link" to='/about' exact>About</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
