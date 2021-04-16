c9b008c30de56c669ce6c505a3456a51ef71f7cf
tscotce <tatyana.celovsky@gmail.com>
2021-04-16 17:44:29 -0400

    Update className

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="App-header">
+    <header className="page-header header container-fluid">
         <h1>Launch Calendar</h1>
     </header>
   );
