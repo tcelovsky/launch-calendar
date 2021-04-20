5f2a98cfc1329eb8c3f4313cd280b05ffc4a7408
tscotce <tatyana.celovsky@gmail.com>
2021-04-20 18:57:13 -0400

    Create basic structure for the Home component

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+        <p>This website provides a summary of the upcoming rocket launches from the Florida space coast.</p>
+    </div>
+  );
+};
+
+export default Home;
