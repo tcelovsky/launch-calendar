237e7ddd07818dab9bdce32ba1bc051e16f0a2af
tscotce <tatyana.celovsky@gmail.com>
2021-04-20 20:17:01 -0400

    Remove last period from the mission sentence

--- frontend/src/components/AddToCalendarButton.js
+++ frontend/src/components/AddToCalendarButton.js
@@ -4,22 +4,29 @@ import { DateTime } from "luxon";
 class AddToCalendarButton extends Component {
   
   parseDate = (date) => {
-      const dateInMillis = Date.parse(date)
-      return DateTime.fromMillis(dateInMillis).toISODate()
+    const dateInMillis = Date.parse(date)
+    return DateTime.fromMillis(dateInMillis).toISODate()
   }
 
   parseTime = (time) => {
-      switch(time) {
-            case 'TBD':
-                return '8:00 am';
-            default:
-                return time;
-      }
+    switch(time) {
+        case 'TBD':
+            return '8:00 am';
+        default:
+            return time;
+    }
+  }
+
+  concatMission = (str) => {
+    if (str[str.length-1] === ".")
+    str = str.slice(0,-1);
+    return str
   }
 
   render() {
     const date = this.parseDate(this.props.date)
     const time = this.parseTime(this.props.time)
+    const mission = this.concatMission(this.props.mission)
 
     return (
         <div title="Add to Calendar" className="addeventatc">
@@ -27,7 +34,7 @@ class AddToCalendarButton extends Component {
             <span className="start">{date} {time}</span>
             <span className="timezone">America/New_York</span>
             <span className="title">{this.props.rocket} Launch</span>
-            <span className="description">{this.props.mission} from {this.props.site}.</span>
+            <span className="description">{mission} from {this.props.site}.</span>
         </div>
     );
     };
