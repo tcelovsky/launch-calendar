bb2b64c0093560c732beaf8b15abdc54d08febbd
tscotce <tatyana.celovsky@gmail.com>
2021-04-20 20:01:55 -0400

    Add Welcome! language to the home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home-link">
+        <h3>Welcome!</h3>
         <h4>Click here to see the list of upcoming rocket launches.</h4>
     </div>
   );
