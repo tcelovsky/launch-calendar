9c0682706728338cd282b11b3afa17f192f12291
tscotce <tatyana.celovsky@gmail.com>
2021-05-05 13:57:59 -0400

    Update p tag to h5

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about">
-        <p>This website provides a summary of the upcoming rocket launches from the Florida space coast.</p>
+        <h5>This website provides a summary of the upcoming rocket launches from the Florida space coast.</h5>
     </div>
   );
 };
