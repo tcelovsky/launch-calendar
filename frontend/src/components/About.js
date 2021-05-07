fbcf151ef9a35e8e4b795259558277a1ab952e41
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 20:22:41 -0400

    Update About page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about">
-        <h5>This website provides a summary of the upcoming rocket launches from the Florida space coast.</h5>
+        <h5>This website provides a summary of the upcoming rocket launches arount the world.</h5>
     </div>
   );
 };
