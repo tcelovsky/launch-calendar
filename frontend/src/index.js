4fc33b886aafdc1733f644eae46ae2184a75ecc4
tscotce <tatyana.celovsky@gmail.com>
2021-05-13 19:53:37 -0400

    Delete unnedessary index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './styles/index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
