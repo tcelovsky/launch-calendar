1c8dae64665b994a208f1f227042cfa947d9fc6c
tscotce <tatyana.celovsky@gmail.com>
2021-05-21 16:47:21 -0400

    Work on a search component

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import LaunchCard from '../components/LaunchCard';
+
+
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          searchValue: '',
+        //   searchResult: []
+        };
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState(() => {      
+			return {        
+				searchValue: e.target.value     
+			}  
+		})
+    }
+
+    // handleClick = (e) => {
+    //     e.preventDefault();
+    //     // this.search()
+    //     this.props.search(this.state.searchValue)
+    // }
+
+    // search = () => {
+    //     const searchResult = []
+    //     this.props.launches.map(launch => {
+    //         if (launch.date === this.state.searchItems) {
+    //             return searchResult.push(launch)
+    //         }
+    //     })
+    //     this.setState(() => {      
+	// 		return {        
+	// 			searchResult: searchResult    
+	// 		}  
+	// 	})
+    // }
+
+    // generateLaunchCards = () => {
+    //     return this.state.searchResult.map(launch => 
+    //         <Link key={launch.id} to={`/launches/${launch.id}`}>
+    //             {<LaunchCard key={launch.id} launch={launch} />}
+    //         </Link>
+    //     )
+    // }
+
+    render() {
+        return (
+            <div className="search">
+                <form className="search-form">
+                    <input type="text" onChange={(e) => this.handleChange(e)}></input>
+                    <button type="button" className="btn btn-default btn-sm" onClick={(e) => this.props.search(e, this.state.searchValue)}>Search</button>
+                </form>
+            </div>
+        )
+        // if (this.state.searchResult.length > 1) {
+        //     return (
+        //             <div id="launch-cards">
+        //                 {this.generateLaunchCards()}
+        //             </div>
+        //     )
+        // } else {
+        //     return (
+        //         <div className="search">
+        //             <form className="search-form">
+        //                 <input type="text" onChange={(e) => this.handleChange(e)}></input>
+        //                 <button type="button" className="btn btn-default btn-sm" onClick={(e) => this.handleClick(e)}>Search</button>
+        //             </form>
+        //         </div>
+        //     )
+        // }
+    }
+}
+
+export default Search
