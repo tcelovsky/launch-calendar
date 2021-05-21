f673aaadd2d8b24180ccff649a2ea9f66e8f64bc
tscotce <tatyana.celovsky@gmail.com>
2021-05-21 17:22:10 -0400

    Update search function

--- frontend/src/containers/Launches.js
+++ frontend/src/containers/Launches.js
@@ -10,7 +10,7 @@ class Launches extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            launches: [],
+            launches: this.props.launches,
             itemsToShow: itemsToShow
         };
     }
