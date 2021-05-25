c57d114050da2c7ac5c330bae344752d106814e6
tscotce <tatyana.celovsky@gmail.com>
2021-05-25 17:47:24 -0400

    Update fetch URL to backend URL

--- frontend/src/actions/launchActions.js
+++ frontend/src/actions/launchActions.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://launch-calendar.herokuapp.com"
 const LAUNCHES_URL = `${BASE_URL}/launches`
 
 export const fetchLaunches = () => {
