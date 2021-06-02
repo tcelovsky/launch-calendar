3529b167f557fce194dcdae8360095bd98878813
tscotce <tatyana.celovsky@gmail.com>
2021-06-02 11:47:42 -0400

    Add logic for when the button should be displayed

--- frontend/src/components/LoadMoreButton.js
+++ frontend/src/components/LoadMoreButton.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const LoadMoreButton = props => {
-  return (
-    <div className="button-holder">
-        <button type="button" className="btn btn-default btn-sm" onClick={props.handleClick}>
-            <span className="glyphicon" aria-hidden="true"></span>
-            Load More
-        </button>
-    </div>
-  );
+  // Not displaying Load More button if all search results have been loaded.
+  if (props.searchResult > 0 && props.searchResult < props.itemsToShow) {
+    return null;
+  }
+  // Displaying Load More button if not all launch cards have been loaded.
+  if ((props.launches - props.itemsToShow + props.defaultItemsToShow) > props.defaultItemsToShow) {
+    return (
+      <div className="button-holder">
+          <button type="button" className="btn btn-default btn-sm" onClick={props.handleClick}>
+              <span className="glyphicon" aria-hidden="true"></span>
+              Load More
+          </button>
+      </div>
+    )
+  }
+  // Not displaying Load More button if all results/launch cards have been loaded.
+  else {
+    return null;
+  }
 };
 
 export default LoadMoreButton;
