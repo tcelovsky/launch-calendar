ac5ce7d635a6af6a4ca0f71983e60e2f7959df3b
tscotce <tatyana.celovsky@gmail.com>
2021-06-02 11:31:22 -0400

    Add logic to display message only when no more results to load

--- frontend/src/components/LoadedAllResults.js
+++ frontend/src/components/LoadedAllResults.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const LoadedAllResults = props => {
-  return (
-    <div className="message">
-        <p>No more results to show.</p>
-    </div>
-  );
+    // Displaying "No more results to show." message if all launch cards have been loaded.
+    if ((props.launches - props.itemsToShow + props.defaultItemsToShow) < props.defaultItemsToShow) {
+        return (
+            <div className="message">
+                <p>No more results to show.</p>
+            </div>
+        )
+    } 
+    // Displaying "No more results to show." message if all seach results have been loaded.
+    if (props.searchResult > 0 && (props.searchResult - props.itemsToShow) < props.defaultItemsToShow) {
+        return (
+            <div className="message">
+                <p>No more results to show.</p>
+            </div>
+        )
+    }
+    // Not displaying "No more results to show." message if not all results/launch cards have been loaded.
+    else {
+        return null;
+    }
 };
 
 export default LoadedAllResults;
